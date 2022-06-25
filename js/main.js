cf47153a7a63fe89cb402921344769ad58d9edb9
yeard-morshed <yeardmorshed1999@gmail.com>
2022-06-25 12:43:04 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//header Scroll
+let nav =document.querySelector(".navbar");
+window.onscroll=function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add(".header-scrolled");
+
+    }else{
+        nav.classList.remove(".header-scrolled");
+    }
+}
+
+//nav hide
+let navBar=document.querySelectorAll(".nav-link");
+let navCollaps=document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+a.addEventListener("click",function(){
+    navCollaps.classList.remove("show");
+})
+})
+
