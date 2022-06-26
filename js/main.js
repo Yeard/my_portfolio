28db353554979d53a086b3edbe0c462105cb22b0
yeard-morshed <yeardmorshed1999@gmail.com>
2022-06-26 22:49:28 +0600

    4rd commit

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 let nav =document.querySelector(".navbar");
 window.onscroll=function(){
     if(document.documentElement.scrollTop > 20){
-        nav.classList.add(".header-scrolled");
+        nav.classList.add("header-scrolled");
 
     }else{
-        nav.classList.remove(".header-scrolled");
+        nav.classList.remove("header-scrolled");
     }
 }
 
